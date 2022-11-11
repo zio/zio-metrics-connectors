7eb12d1b8ade802bd968e8af988084234d3b840c
Andreas Gies <andreas@wayofquality.de>
2022-11-11 18:45:45 +0100

    Remove ScalaJS support, bump versions, prepare release

--- project/Version.scala
+++ project/Version.scala
@@ -1,10 +1,10 @@
 object Version {
   val Scala211   = "2.11.12"
   val Scala212   = "2.12.17"
-  val Scala213   = "2.13.9"
-  val ScalaDotty = "3.2.0"
+  val Scala213   = "2.13.10"
+  val ScalaDotty = "3.2.1"
 
-  val zio     = "2.0.2"
-  val zioJson = "0.3.0-RC11"
+  val zio     = "2.0.3"
+  val zioJson = "0.3.0"
   val zioHttp = "2.0.0-RC11"
 }
