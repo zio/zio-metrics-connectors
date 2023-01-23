6b7b4ae635d24a85aeacce6eb5deb788518d6a1c
Albert Peto <petoalbert32@gmail.com>
2023-01-23 14:47:36 +0100

    Run scalafmt

--- core/src/test/scala/zio/metrics/connectors/datadog/DatadogEncoderSpec.scala
+++ core/src/test/scala/zio/metrics/connectors/datadog/DatadogEncoderSpec.scala
@@ -12,7 +12,7 @@ object DatadogEncoderSpec extends ZIOSpecDefault {
 
   override def spec = suite("The Datadog encoder should")(
     sendHistogram,
-    sendHistograms
+    sendHistograms,
   ) @@ timed @@ timeoutWarning(60.seconds)
 
   private val sendHistogram = test("send histogram update") {
@@ -25,11 +25,11 @@ object DatadogEncoderSpec extends ZIOSpecDefault {
   }
 
   private val sendHistograms = test("send histogram updates") {
-    val name = "testHistogram"
-    val tagName = "testTag"
+    val name     = "testHistogram"
+    val tagName  = "testTag"
     val tagValue = "tagValue"
-    val key = MetricKey.histogram(name, Boundaries(Chunk.empty)).tagged(tagName, tagValue)
-    val encoded = new String(DatadogEncoder.encodeHistogramValues(key, NonEmptyChunk(1.0, 2.0)).toArray)
+    val key      = MetricKey.histogram(name, Boundaries(Chunk.empty)).tagged(tagName, tagValue)
+    val encoded  = new String(DatadogEncoder.encodeHistogramValues(key, NonEmptyChunk(1.0, 2.0)).toArray)
     assert(encoded)(equalTo(s"$name:1:2|d|#$tagName:$tagValue"))
   }
 }
